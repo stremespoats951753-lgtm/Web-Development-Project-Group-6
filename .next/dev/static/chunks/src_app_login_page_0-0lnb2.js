a940c7dc1be03e7db5a100e0231d413ad390aec8
ah <your.email@domain.com>
2026-05-02 23:29:37 -0400

    delete misc .next file

--- .next/dev/static/chunks/src_app_login_page_0-0lnb2.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_06x337o._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_stats_page_0-0lnb2.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_10lr3.s._.js"],
-    source: "dynamic"
-});
